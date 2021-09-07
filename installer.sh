4a25b06c042dc497557960e2d96a02555a9981ea
Yury Bragin <ybragin22@gmail.com>
2021-09-07 20:39:18 +0300

    Delete installer.sh

--- installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Разворачиваем контейнеры"
-docker-compose up -d --build
-echo "Контейнеры запущены"
-echo "Делаем миграции"
-docker-compose exec web python3 manage.py migrate auth
-docker-compose exec web python3 manage.py migrate --run-syncdb
-echo "Собираем статику"
-docker-compose exec web python manage.py collectstatic --no-input
